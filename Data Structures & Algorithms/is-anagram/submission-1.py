64ea09b72a2f0ac4fbfc74971883b453a9e1a0b7
anurag04-max <75245612+anurag04-max@users.noreply.github.com>
2026-07-20 22:03:48 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False      
+        hash_mp = {}
+        for i in range(len(s)):
+            hash_mp[s[i]] = hash_mp.get(s[i],0) + 1
+        for i in range(len(t)):
+            if t[i] not in hash_mp:
+                return False
+            hash_mp[t[i]] = hash_mp[t[i]] - 1
+            if hash_mp[t[i]] == 0:
+                del hash_mp[t[i]] 
+        if len(hash_mp) > 0:
+            return False
+        return True               
+        
